a100382247321de449af94aaf6fe5c707439b715
Erasmo Bezerra <erasmo.ads.tech@gmail.com>
2023-03-18 08:54:25 -0300

    update endpoint findById

--- src/main/java/com/crud/controllers/PessoaController.java
+++ src/main/java/com/crud/controllers/PessoaController.java
@@ -3,9 +3,11 @@ package com.crud.controllers;
 import com.crud.Model.Pessoa;
 import com.crud.dto.PessoaDTO;
 import com.crud.repositories.PessoaRepository;
+import com.crud.services.PessoaService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -13,6 +15,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import java.util.List;
 
+@Validated
 @RestController
 @AllArgsConstructor
 @RequestMapping("/V1/pessoas")
@@ -21,10 +24,8 @@ public class PessoaController {
     private final PessoaService pessoaService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<Pessoa> findById(@PathVariable @NotNull @Positive Integer id) {
-        return pessoaService.findById(id)
-                .map(recordFound -> ResponseEntity.ok().body(recordFound))
-                .orElse(ResponseEntity.notFound().build());
+    public PessoaDTO findById(@PathVariable @NotNull @Positive Integer id) {
+        return pessoaService.findById(id);
 
     }
 
